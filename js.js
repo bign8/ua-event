3ea247165f242042f66068c18c20bbee5df1a100
bign8-AZ <nwoods@azworld.com>
2014-04-29 15:58:39 -0600

    Adding maps to event location

--- js.js
+++ js.js
@@ -1,5 +1,71 @@
 "use strict";
 
+/* ------------------------------------------------------------------- *|
+ * Google Maps Wrapper (Nathan Woods: April 28, 2014)
+ * docs: https://developers.google.com/maps/documentation/javascript/reference
+ * ------------------------------------------------------------------- */
+
+// Constructor
+var ELA_MAP = function (ele, opt, name, txt) {
+	this.ele = ele;
+	this.opt = opt;
+	this.name = name;
+	this.txt = txt;
+	ELA_MAP._instances.push(this);
+	this.init();
+};
+
+// Global Object Attributes
+ELA_MAP._instances  = [];
+
+// Global Object functions
+ELA_MAP.google_maps_loaded = function() {
+	for (var i = 0; i < ELA_MAP._instances.length; i++)
+		ELA_MAP._instances[i].init();
+};
+ELA_MAP.load_google = function() {
+	if (ELA_MAP.map_src) return;
+	ELA_MAP.map_src  = document.createElement('script');
+	ELA_MAP.map_src.type = 'text/javascript';
+	ELA_MAP.map_src.src  = 'https://maps.googleapis.com/maps/api/js?v=3.exp&sensor=false&callback=ELA_MAP.google_maps_loaded&key=AIzaSyC7fARB8SaSEgUDUG6aFfqEutUNCTPzzYE';
+	document.body.appendChild(ELA_MAP.map_src);
+};
+
+// Instances
+ELA_MAP.prototype = {
+	init: function() {
+		if (!ELA_MAP.map_src) return ELA_MAP.load_google();
+
+		// Generate new map
+		this.opt.center = new google.maps.LatLng(this.opt.lat, this.opt.lng);
+		this.map = new google.maps.Map(this.ele, this.opt);
+
+		// Generate new marker
+		this.marker = new google.maps.Marker({
+			animation: google.maps.Animation.DROP,
+			position:  this.opt.center,
+			title:     this.name,
+			map:       this.map,
+		});
+
+		// InfWwindow
+		if (this.txt) {
+			this.info = new google.maps.InfoWindow({ content: this.txt, maxWidth: 600 });
+
+			var cb = function() {
+				this.info.open(this.map, this.marker);
+			};
+			google.maps.event.addListener(this.marker, 'click', cb.bind(this));
+			cb.call(this);
+		}
+	}
+};
+
+/* ------------------------------------------------------------------- *|
+ * jQuery page accents
+ * docs:  http://api.jquery.com/
+ * ------------------------------------------------------------------- */
+
 // jQuery soft scroll
 jQuery.fn.scroll_top = function (cb) {
 	if ( this.offset() ) jQuery('html, body').animate({
