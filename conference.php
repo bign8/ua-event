070e5724cd15732484a80558ff1d39c4ad8c540a
bign8-AZ <nwoods@azworld.com>
2014-05-08 17:08:48 -0600

    Init Editor

--- conference.php
+++ conference.php
@@ -7,17 +7,33 @@
 	if (false === ($event = $app->get_conf($_REQUEST['slug']))) throw new Exception('Un-found event');
 	$title = $event['title'];
 	include('tpl/head.tpl.html');
+
+	$is_edit = isset($_REQUEST['edit']);
+	$path = $is_edit ? 'conf-edit' : 'conf';
+	$wrap = $is_edit ? 'form' : 'div';
 ?>
 
-<div id="wrap">
-	<?php $panel_include('tpl/conf/home.frame.html',      'home',      $event); ?>
-	<?php $panel_include('tpl/conf/about.frame.html',     'about',     $event); ?>
-	<?php $panel_include('tpl/conf/location.frame.html',  'location',  $event); ?>
-	<?php if (count($event['speakers']) > 0) $panel_include('tpl/conf/speakers.frame.html',  'speakers',  $event); ?>
-	<?php if (count($event['agenda']) > 0)   $panel_include('tpl/conf/agenda.frame.html',    'agenda',    $event); ?>
-	<?php $panel_include('tpl/conf/attendees.frame.html', 'attendees', $event); ?>
-	<?php $panel_include('tpl/conf/sponsors.frame.html',  'sponsors',  $event); ?>
-</div><!-- ./wrap -->
+<?php if ($is_edit): ?>
+	<script src="//tinymce.cachefly.net/4.0/tinymce.min.js"></script>
+	<script src="./edit.js"></script>
+	<script>
+		tinymce.init({
+			selector: 'textarea',
+			menubar: false,
+			toolbar_items_size: 'small',
+		});
+	</script>
+<?php endif; ?>
+
+<<?=$wrap; ?> id="wrap">
+	<?php $panel_include('tpl/' . $path . '/home.frame.html',      'home',      $event); ?>
+	<?php $panel_include('tpl/' . $path . '/about.frame.html',     'about',     $event); ?>
+	<?php $panel_include('tpl/' . $path . '/location.frame.html',  'location',  $event); ?>
+	<?php if (count($event['speakers']) > 0 || $is_edit) $panel_include('tpl/' . $path . '/speakers.frame.html',  'speakers',  $event); ?>
+	<?php if (count($event['agenda']) > 0 || $is_edit)   $panel_include('tpl/' . $path . '/agenda.frame.html',    'agenda',    $event); ?>
+	<?php $panel_include('tpl/' . $path . '/attendees.frame.html', 'attendees', $event); ?>
+	<?php $panel_include('tpl/' . $path . '/sponsors.frame.html',  'sponsors',  $event); ?>
+</<?=$wrap; ?>><!-- ./wrap -->
 
 
 <?php include('tpl/foot.tpl.html'); ?>
---
+++ edit.js
@@ -0,0 +1,61 @@
+"use strict";
+
+/* ------------------------------------------------------------------- *|
+ * Google Maps Wrapper (Nathan Woods: April 28, 2014)
+ * docs: https://developers.google.com/maps/documentation/javascript/reference
+ * ------------------------------------------------------------------- */
+
+// Constructor
+var ELA_MAP_EDIT = function (ele, opt, name) {
+	this.ele = ele;
+	this.opt = opt;
+	this.name = name;
+	ELA_MAP_EDIT._instances.push(this);
+	this.init();
+};
+
+// Global Object Attributes
+ELA_MAP_EDIT._instances  = [];
+
+// Global Object functions
+ELA_MAP_EDIT.google_maps_loaded = function() {
+	for (var i = 0; i < ELA_MAP_EDIT._instances.length; i++)
+		ELA_MAP_EDIT._instances[i].init();
+};
+ELA_MAP_EDIT.load_google = function() {
+	if (ELA_MAP_EDIT.map_src) return;
+	ELA_MAP_EDIT.map_src  = document.createElement('script');
+	ELA_MAP_EDIT.map_src.type = 'text/javascript';
+	ELA_MAP_EDIT.map_src.src  = 'https://maps.googleapis.com/maps/api/js?v=3.exp&sensor=false&callback=ELA_MAP_EDIT.google_maps_loaded&key=AIzaSyC7fARB8SaSEgUDUG6aFfqEutUNCTPzzYE';
+	document.body.appendChild(ELA_MAP_EDIT.map_src);
+};
+
+// Instances
+ELA_MAP_EDIT.prototype = {
+	init: function() {
+		if (!ELA_MAP_EDIT.map_src) return ELA_MAP_EDIT.load_google();
+
+		// Generate new map
+		// this.opt.center = new google.maps.LatLng(this.opt.lat, this.opt.lng);
+		this.map = new google.maps.Map(this.ele, this.opt);
+
+		// Generate new marker
+		this.marker = new google.maps.Marker({
+			animation: google.maps.Animation.DROP,
+			position:  this.opt.center,
+			title:     this.name,
+			map:       this.map,
+		});
+
+		// InfWwindow
+		if (this.txt) {
+			this.info = new google.maps.InfoWindow({ content: this.txt, maxWidth: 600 });
+
+			var cb = function() {
+				this.info.open(this.map, this.marker);
+			};
+			google.maps.event.addListener(this.marker, 'click', cb.bind(this));
+			cb.call(this);
+		}
+	}
+};
