f65e6e539896d7b8a9d8e6b57598c8384b8d454e
bign8-AZ <nwoods@azworld.com>
2014-05-21 15:20:40 -0600

    Adding tinymce to profile page

--- profile.php
+++ profile.php
@@ -4,6 +4,17 @@
 	include('tpl/parts/head.tpl.html');
 ?>
 
+<link rel="stylesheet" href="//cdnjs.cloudflare.com/ajax/libs/tinymce/4.0.21/skins/lightgray/skin.min.css">
+<script src="//cdnjs.cloudflare.com/ajax/libs/tinymce/4.0.21/tinymce.min.js"></script>
+<script src="//cdnjs.cloudflare.com/ajax/libs/tinymce/4.0.21/themes/modern/theme.min.js"></script>
+<script type="text/javascript">
+	tinymce.init({
+		selector: 'textarea',
+		menubar: false,
+		toolbar_items_size: 'small'
+	});
+</script>
+
 <div id="wrap">
 	<?php $panel_include('tpl/frames/profile.frame.html', 'home'); ?>
 </div><!-- ./wrap -->
